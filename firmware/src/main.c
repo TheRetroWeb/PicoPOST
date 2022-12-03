e9387def6771e6f22c2591b0d83362febf2520a0
TheRealZago <enrico.zaghini@outlook.it>
2022-12-03 19:35:36 +0100

    GPIO was the wrong approach - Removed the GPIO-based ISA bus handler library. Way too slow! - Brand new PIO-based read logic. Wicked fast!
    Smarter application handling
    - Provisioning for logic plugin selection
    - Implemented multi-core, queued serial print for Port 80h readout

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,69 +1,95 @@
 /**
-  *    src/main.c
-  * 
-  *    Well, main!
-  * 
-  **/
+ *    src/main.c
+ *
+ *    Well, main!
+ *
+ **/
 
 #include "proj.h"
 
-#include <stdio.h>
+#include "hardware/pio.h"
+#include "pico/multicore.h"
 #include "pico/stdlib.h"
+#include "pico/util/queue.h"
+#include <stdio.h>
 
 //#include "oled.h"
-#include "bus.h"
+#include "fastread.pio.h"
+#include "main.h"
 
-int main() {
-    // Local variables
-    uint16_t currAddress = 0x0000;
-    uint8_t currData = 0x00;
-    uint8_t firstReset = 0x00;
+queue_t postList;
+static volatile bool quitLoop = false;
 
-    // HW/FW initialization
-    stdio_init_all();
-    //OLED_Init();
-    //OLED_ClearScreen();
-    Bus_Init();
+void WriteSerial()
+{
+    while (!quitLoop) {
+        int count = queue_get_level(&postList);
+        if (count > 0) {
+            uint8_t buffer = 0x00;
+            queue_remove_blocking(&postList, &buffer);
+            printf("%02X ", buffer);
+        }
+    }
+}
+
+void Logic_Port80Reader(PIO pio)
+{
+    uint offset = pio_add_program(pio, &Bus_FastRead_program);
+    uint sm = pio_claim_unused_sm(pio, true);
+    Bus_FastRead_init(pio, sm, offset);
+
+    uint8_t data = 0xFF;
+    uint32_t fullRead = 0x0000;
+    quitLoop = false;
+    multicore_launch_core1(WriteSerial);
+    while (!quitLoop) {
+        // The readout from the FIFO should look a bit like this
+        // |  A[7:0]  |  D[7:0]  |  A[15:8]  |  DC  |
+
+        fullRead = Bus_FastRead_PinImage(pio, sm);
+        uint16_t addr = (fullRead & 0x0000FF00) + ((fullRead & 0xFF000000) >> 24);
+        if (addr == 0x0080) {
+            uint8_t data = (fullRead & 0x00FF0000) >> 16;
+            queue_try_add(&postList, &data);
+        }
+    }
+    pio_sm_set_enabled(pio, sm, false);
+    pio_sm_restart(pio, sm);
+    pio_sm_unclaim(pio, sm);
+    pio_remove_program(pio, &Bus_FastRead_program, offset);
+}
+
+int main()
+{
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-    gpio_put(PICO_DEFAULT_LED_PIN, true);    
+    gpio_put(PICO_DEFAULT_LED_PIN, false);
+    stdio_init_all();
+
+    PIO pio = pio0;
+    ProgramSelect logicSelect = PS_None;
+    queue_init(&postList, sizeof(uint8_t), MAX_QUEUE_LENGTH);
 
     // Greetings!
     printf("-- PicoPOST v%d.%d.%d --\n", PROJ_MAJ_VER, PROJ_MIN_VER, PROJ_CDB_VER);
     printf("HW by fire219, SW by Zago\n");
+    gpio_put(PICO_DEFAULT_LED_PIN, true);
 
-    // Readout loop
-    while(1) {
-        // Skip readout loop while bus is resetting
-        // Also reset our internal data
-        while (Bus_IsResetting()) {
-            if (!firstReset) {
-                printf("! Resetting\n");
-                firstReset = 1;
-            }
-            currAddress = 0x0000;
-            currData = 0x00;
+    while (1) {
+        // TODO: implement ui selection for use mode
+        // should involve oled and buttons
+        logicSelect = PS_Port80Reader;
+
+        // pressing a certain key combo should also trigger a logic exit condition
+        switch (logicSelect) {
+        case PS_Port80Reader: {
+            Logic_Port80Reader(pio);
+        } break;
+
+        default: {
+            // come again?
+        } break;
         }
-        firstReset = 0;
-
-        // If upper A[16..19] are low and ~IOW and ~AEN are active, read upper 8 bits
-        if (Bus_IsReady()) {
-            // If upper 8 bits are 0x00, read lower 8 bits
-            if (Bus_GetAddressUpper() == 0x00) {
-                // If lower 8 bits are 0x80, we're in business
-                if (Bus_GetAddressLower() == 0x80) {
-                    uint8_t newData = Bus_GetData();
-                    if (newData != currData) {
-                        currData = newData;
-                        printf("> New code, %02Xh\n", currData);
-                    }
-                }
-            }
-        }
-
-        // TODO Made Bus_Ready an interrupt. Polling doesn't sound very precise
-
-        // Rinse and repeat
     }
 
     return 0;
---
+++ firmware/src/main.h
@@ -0,0 +1,42 @@
+/**
+  *    src/main.h
+  * 
+  *    Some handy info for running the main application
+  * 
+  **/
+
+#ifndef __PICOPOST_SRC_MAIN
+#define __PICOPOST_SRC_MAIN
+
+// Serial is slow, but I hope we can make do without filling up 512 bytes of
+// bus data in less than we can empty the serial buffer at 115200 bps
+#define MAX_QUEUE_LENGTH 512
+
+typedef enum __picopost_main_progselect {
+    PS_None,
+
+    PS_Port80Reader,
+    PS_VoltageMonitor
+
+    // More stuff coming soon?
+} ProgramSelect;
+
+/**
+ * @brief Reads I/O port 80h and returns data to the reader queue.
+ * 
+ * @param pio The PIO instance to use for the read operation.
+ * 
+ * @par
+ * This is the primary target for this project.
+ * To accomplish this task, one of the two PIO engines in the RP2040 is used
+ * to listen for the Bus_Ready signal to come up, then it samples the address
+ * and data busses in one shot. The read operation itself is so fast, the ISA
+ * bus itself could be read 4 times in a single host transaction!
+ * Data is then sent to the queue, so the second core can waste time for
+ * printing it to USB CDC serial port. OLED output will be implemented in the
+ * near future.
+ * 
+ */
+void Logic_Port80Reader(PIO pio);
+
+#endif // __PICOPOST_SRC_MAIN
